068793cf1a3ba2126a6105a78a8915a6a6cdf0ad
a.oroumand <a.oroumand@dotin.ir>
2023-11-29 08:54:21 +0330

    add datetime contoller

---
+++ WebAPI02/Controllers/DateTimeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI02.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DateTimeController : Controller
+    {
+
+        [HttpGet(Name = "Get")]
+        public IActionResult Index()
+        {
+            return Ok(new
+            {
+                Value = DateTime.Now,
+            });
+        }
+    }
+}
